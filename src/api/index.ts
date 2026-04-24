4a69e766dc60f8170c8f04a3f53e519f83bb08d2
servonnm <mat.servonnet@gmail.com>
2026-04-25 00:09:44 +0200

    Modif lien avec Back

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ import type {
 } from '../types'
 
 const api = axios.create({
-   baseURL: 'https://gymtracker-api-1kin.onrender.com',
+   baseURL: 'https://gymtracker-api-1kin.onrender.com/api',
 })
 
 // ---- Users ----
